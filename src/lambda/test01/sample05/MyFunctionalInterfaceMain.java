be030b7edc429e3ec92d21f08959e0a788215e79
gksruf2848 <4455qqq@naver.com>
2022-04-02 23:49:24 +0900

    .

--- src/lambda/test01/sample05/MyFunctionalInterfaceMain.java
+++ src/lambda/test01/sample05/MyFunctionalInterfaceMain.java
@@ -1,4 +1,9 @@
 package lambda.test01.sample05;
 
 public class MyFunctionalInterfaceMain {
+    public static void main(String[] args) {
+        UsingThis usingThis = new UsingThis();
+        UsingThis.Inner inner = usingThis.new Inner();
+        inner.method();
+    }
 }
--- src/lambda/test01/sample05/UsingThis.java
+++ src/lambda/test01/sample05/UsingThis.java
@@ -4,14 +4,18 @@ public class UsingThis {
 
     public int outterField = 10;
 
-    class inner {
+    class Inner {
         int innerField = 20;
 
         void method() {
             MyFunctionalInterface fi = () -> {
+                System.out.println("outterField : " + outterField);
+                System.out.println("outterField : " + UsingThis.this.outterField);
 
-
-            }
+                System.out.println("innerField : " + innerField);
+                System.out.println("innerField : " + this.innerField);
+            };
+            fi.method();
         }
     }
 }
